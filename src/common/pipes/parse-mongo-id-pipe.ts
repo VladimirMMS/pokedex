ee57b66b37633ccbe3274c8ac0dc5e07203378a1
Vladimir Mercado <vladimirmercado2001@gmail.com>
2022-08-24 10:51:53 -0400

    pokedex

---
+++ src/common/pipes/parse-mongo-id-pipe.ts
@@ -0,0 +1,18 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+import { isValidObjectId } from 'mongoose';
+
+@Injectable()
+export class ParseMongoIdPipe implements PipeTransform {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  transform(value: string, metadata: ArgumentMetadata) {
+    if (!isValidObjectId(value)) {
+      throw new BadRequestException(`The id ${value} needs to be a mongoId`);
+    }
+    return value;
+  }
+}
---
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -0,0 +1,12 @@
+import { IsInt, IsPositive, MinLength, IsString, Min } from 'class-validator';
+
+export class CreatePokemonDto {
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  no: number;
+
+  @IsString()
+  @MinLength(1)
+  name: string;
+}
